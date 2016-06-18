5649e1ebc8a73375085d232f92833ef917d81fab
Milton Stafford <mstafford2012@my.fit.edu>
2016-06-17 22:45:40 -0400

    add blank aliases and mail store

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 USER=`whoami`
 sudo -s
+chmod +x simplemail
 touch /opt/SimpleMail
 cp Client.py Server.py simplemail_aliases simplemail_store SimpleMail_lib.py /opt/SimpleMail
-cp simplemail /home/${USER}/.bin/
+cp simplemail /usr/local/bin
 cp sm-server.conf /etc/init/
 initctl reload-configuration
 initctl start sm-server
